09874af93b010aaf35b41abd8ee11a4d9316979c
Mike <mike@vettvangur.is>
2025-07-01 19:03:16 +0000

    animated header unicorns + header navigation siziing & container fix

--- src/scripts/layout/header.ts
+++ src/scripts/layout/header.ts
@@ -5,6 +5,7 @@ const header = {
   el: {
     header: document.querySelector<HTMLElement>('.header'),
     text: document.querySelectorAll<HTMLElement>('.header__headline, .header__text'),
+    unicorn: document.querySelector<HTMLElement>('.header__unicorn'),
   },
 
   init(): void {
@@ -24,6 +25,49 @@ const header = {
       scrub: true,
       start: '0% -40rem',
       trigger: this.el.header,
+    })
+
+    this.unicornAnimation()
+  },
+
+  unicornAnimation(): void {
+    const animate = ({ animationProps, end = '+=120rem', page, start = '0% 0%' }) => {
+      if (window.location.href.match(page)) {
+        ScrollTrigger.create({
+          animation: gsap.to(this.el.unicorn, { ...animationProps, ease: 'none' }),
+          end,
+          scrub: true,
+          start,
+          trigger: this.el.header,
+        })
+      }
+    }
+
+    animate({
+      animationProps: { x: '-50%', y: '-20%' },
+      end: '+=90rem',
+      page: '/home',
+    })
+
+    animate({
+      animationProps: { y: '28%' },
+      page: '/search',
+    })
+
+    animate({
+      animationProps: { x: '48%', y: '81%', rotation: 21 },
+      page: '/cart',
+    })
+
+    animate({
+      animationProps: { x: '78%', y: '31%' },
+      page: '/favorites',
+    })
+
+    animate({
+      animationProps: { x: '78%' },
+      end: '+=90rem',
+      page: '/messages',
     })
   },
 }
